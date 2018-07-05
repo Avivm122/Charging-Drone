87d5b367d3b12a6d18aa0a65b5ce18d6e4f8e77c
Avivm122 <37801256+Avivm122@users.noreply.github.com>
2018-07-05 14:35:28 +0300

    Add files via upload

---
+++ Libs/DJI-Onboard-sdk/build/projects/linux/squared-flight/CMakeFiles/djiosdk-squared-flight.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aviv-kcg/Desktop/Drone-Project/projects/linux/common/dji_linux_environment.cpp" "/home/aviv-kcg/Desktop/Drone-Project/build/projects/linux/squared-flight/CMakeFiles/djiosdk-squared-flight.dir/__/common/dji_linux_environment.cpp.o"
+  "/home/aviv-kcg/Desktop/Drone-Project/projects/linux/common/dji_linux_helpers.cpp" "/home/aviv-kcg/Desktop/Drone-Project/build/projects/linux/squared-flight/CMakeFiles/djiosdk-squared-flight.dir/__/common/dji_linux_helpers.cpp.o"
+  "/home/aviv-kcg/Desktop/Drone-Project/projects/linux/squared-flight/flight_control_sample.cpp" "/home/aviv-kcg/Desktop/Drone-Project/build/projects/linux/squared-flight/CMakeFiles/djiosdk-squared-flight.dir/flight_control_sample.cpp.o"
+  "/home/aviv-kcg/Desktop/Drone-Project/projects/linux/squared-flight/main.cpp" "/home/aviv-kcg/Desktop/Drone-Project/build/projects/linux/squared-flight/CMakeFiles/djiosdk-squared-flight.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../projects/linux/../../osdk-core/api/inc"
+  "../projects/linux/../../osdk-core/utility/inc"
+  "../projects/linux/../../osdk-core/hal/inc"
+  "../projects/linux/../../osdk-core/protocol/inc"
+  "../projects/linux/../../osdk-core/platform/linux/inc"
+  "../projects/linux/squared-flight/../common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/aviv-kcg/Desktop/Drone-Project/build/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Libs/DJI-Onboard-sdk/build/projects/linux/squared-flight/CMakeFiles/djiosdk-squared-flight.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/djiosdk-squared-flight.dir/flight_control_sample.cpp.o"
+  "CMakeFiles/djiosdk-squared-flight.dir/main.cpp.o"
+  "CMakeFiles/djiosdk-squared-flight.dir/__/common/dji_linux_environment.cpp.o"
+  "CMakeFiles/djiosdk-squared-flight.dir/__/common/dji_linux_helpers.cpp.o"
+  "../../../bin/djiosdk-squared-flight.pdb"
+  "../../../bin/djiosdk-squared-flight"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/djiosdk-squared-flight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
